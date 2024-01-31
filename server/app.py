4a568d6be0745ed0cb75bbae18a2a2a1f5f22f2d
Bruno Rossi <brunogabrielrossi@gmail.com>
2024-01-31 13:54:06 -0500

    Submitting

--- server/app.py
+++ server/app.py
@@ -14,13 +14,64 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
 
-@app.route('/messages/<int:id>')
+    if request.method == 'GET':
+        messages = []
+        
+        for message in Message.query.order_by(Message.created_at).all():
+
+            message_dict = message.to_dict()
+            messages.append(message_dict)
+
+        response = make_response(
+            messages,
+            200
+        )
+
+        return response
+    elif request.method == 'POST':
+
+        request_data = request.get_json()
+
+        new_message = Message(
+            body=request_data['body'],
+            username=request_data['username']
+        )
+
+        db.session.add(new_message)
+        db.session.commit()
+
+        new_message_dict = new_message.to_dict()
+
+        return make_response( new_message_dict, 201 )
+
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter(Message.id == id).first()
+
+    if request.method == 'PATCH':
+        for attr in request.get_json():
+            setattr(message, attr, request.get_json()[attr])
+        
+        db.session.add(message)
+        db.session.commit()
+
+        message_dict = message.to_dict()
+        return make_response ( message_dict, 200 )
+
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+
+    response_body = {
+            "delete_successful": True,
+            "message": "Baked good deleted."
+        } 
+
+    return make_response( response_body, 200 )
+
 
 if __name__ == '__main__':
     app.run(port=5555)
